2417a3f8ea28bdde3625cd0043236fc15d451ba3
Roj Serbest <rojserbest@gmail.com>
2021-07-09 00:17:54 +0300

    Fix ignore plugin

--- plugins/ignore.py
+++ plugins/ignore.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 
 @Client.on_message(filters.regex("#(реклам(а|)|advertisement)"), group=-10)
-async def ignore(bot: Client, _):
-    await bot.stop_propagation()
+async def ignore(_, message: Message):
+    await message.stop_propagation()
