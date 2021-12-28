2d22919350d71f6967b527bcdf79ac408b7dd029
Roj <ez@roj.im>
2021-12-28 14:36:06 +0300

    Add default value for `DB_URI`

--- env.ts
+++ env.ts
@@ -3,5 +3,5 @@ import { cleanEnv, str } from "https://deno.land/x/envalid/mod.ts";
 
 export default cleanEnv(Deno.env.toObject(), {
   BOT_TOKEN: str(),
-  DB_URI: str(),
+  DB_URI: str({ default: "mongodb://localhost" }),
 });
