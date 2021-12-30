4b70306738481e965a1f0296c5b5fab1469b6791
Roj <ez@roj.im>
2021-12-30 16:04:35 +0300

    Fix TypeScript issues

--- handlers/mod.ts
+++ handlers/mod.ts
@@ -1,9 +1,10 @@
 import { Composer } from "https://deno.land/x/grammy/mod.ts";
+import { Context } from "../context.ts";
 import translate from "./translate.ts";
 import callback from "./callback.ts";
 import sudoers from "./sudoers.ts";
 
-const composer = new Composer();
+const composer = new Composer<Context>();
 
 export default composer;
 
--- mod.ts
+++ mod.ts
@@ -1,10 +1,11 @@
 import { Bot } from "https://deno.land/x/grammy/mod.ts";
 import { hydrateFiles } from "https://deno.land/x/grammy_files/mod.ts";
+import { Context } from "./context.ts";
 import handlers from "./handlers/mod.ts";
 import { log } from "./utils.ts";
 import env from "./env.ts";
 
-const bot = new Bot(env.BOT_TOKEN);
+const bot = new Bot<Context>(env.BOT_TOKEN);
 
 bot.catch(({ error }) => log(String(error), "warning"));
 
@@ -12,7 +13,12 @@ bot.api.config.use(hydrateFiles(bot.token));
 
 bot.use(handlers);
 
-await bot.api.getMe();
+try {
+  await bot.api.getMe();
+} catch (err) {
+  log(`Failed to start the bot: ${err}`, "error");
+  Deno.exit();
+}
 
 log("The bot is running.", "secondary");
 
