b7bd72a16bf0145268d436633b670135483df074
Roj <ez@roj.im>
2021-12-30 10:14:53 +0300

    Tell when the bot is running

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { Bot } from "https://deno.land/x/grammy/mod.ts";
 import { hydrateFiles } from "https://deno.land/x/grammy_files/mod.ts";
 import handlers from "./handlers/mod.ts";
+import { log } from "./utils.ts";
 import env from "./env.ts";
 
 const bot = new Bot(env.BOT_TOKEN);
@@ -10,5 +11,9 @@ bot.catch(console.error);
 bot.api.config.use(hydrateFiles(bot.token));
 
 bot.use(handlers);
+
+await bot.api.getMe();
+
+log("The bot is running.", "secondary");
 
 await bot.start();
