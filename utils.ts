d15b839c2603d975b0de280fad2b9449f290cc2e
Roj <ez@roj.im>
2022-01-01 00:20:04 +0300

    Add unparse util

--- utils.ts
+++ utils.ts
@@ -10,6 +10,7 @@ import { Context, GrammyError } from "https://deno.land/x/grammy/mod.ts";
 import {
   InlineKeyboardButton,
   InlineKeyboardMarkup,
+  MessageEntity,
 } from "https://cdn.skypack.dev/@grammyjs/types@v2.5.0?dts";
 import { Language, languages } from "./data.ts";
 
@@ -35,10 +36,11 @@ export async function findLanguage(
   ctx: Context & {
     chat: NonNullable<Context["chat"]>;
     from: NonNullable<Context["from"]>;
-  },
+  }
 ): Promise<Language & { id: string }> {
-  const filteredLanguage =
-    Object.entries(languages).filter(([_, { edit }]) => edit == ctx.chat.id)[0];
+  const filteredLanguage = Object.entries(languages).filter(
+    ([_, { edit }]) => edit == ctx.chat.id
+  )[0];
 
   if (typeof filteredLanguage === "undefined") {
     await answer(ctx, "Language not found.");
@@ -57,25 +59,25 @@ export async function findLanguage(
 
 export function removeButton(
   replyMarkup: InlineKeyboardMarkup,
-  callbackData: string,
+  callbackData: string
 ) {
   for (const x in replyMarkup.inline_keyboard) {
     for (const y in replyMarkup.inline_keyboard[x]) {
       if (
-        (<InlineKeyboardButton.CallbackButton> replyMarkup
-          .inline_keyboard[x][y]).callback_data == callbackData
+        (<InlineKeyboardButton.CallbackButton>replyMarkup.inline_keyboard[x][y])
+          .callback_data == callbackData
       ) {
         delete replyMarkup.inline_keyboard[x][y];
       }
     }
 
     replyMarkup.inline_keyboard[x] = replyMarkup.inline_keyboard[x].filter(
-      (y) => y,
+      (y) => y
     );
   }
 
-  replyMarkup.inline_keyboard = replyMarkup.inline_keyboard.filter((x) =>
-    x.length != 0
+  replyMarkup.inline_keyboard = replyMarkup.inline_keyboard.filter(
+    (x) => x.length != 0
   );
 }
 
@@ -83,13 +85,13 @@ export function replaceButton(
   replyMarkup: InlineKeyboardMarkup,
   currentCallbackData: string,
   newText: string | ((current: string) => string),
-  newCallbackData: string,
+  newCallbackData: string
 ) {
   for (const i in replyMarkup.inline_keyboard) {
     const x = replyMarkup.inline_keyboard[i];
 
     for (const i in x) {
-      const y = <InlineKeyboardButton.CallbackButton> x[i];
+      const y = <InlineKeyboardButton.CallbackButton>x[i];
 
       if (y.callback_data == currentCallbackData) {
         y.text = typeof newText === "string" ? newText : newText(y.text);
@@ -108,26 +110,21 @@ const variants = {
   error: (str: string) => bgRed(black(str)),
 };
 
-export function log(
-  text: string,
-  variant: keyof typeof variants = "normal",
-) {
+export function log(text: string, variant: keyof typeof variants = "normal") {
   text = new Date().toLocaleTimeString().slice(0, -3) + ": " + text;
 
-  console.log(
-    variants[variant](text),
-  );
+  console.log(variants[variant](text));
 }
 
 export function hasButton(
   { inline_keyboard }: InlineKeyboardMarkup,
-  callbackData: string,
+  callbackData: string
 ) {
   for (const i in inline_keyboard) {
     const x = inline_keyboard[i];
 
     for (const i in x) {
-      const y = <InlineKeyboardButton.CallbackButton> x[i];
+      const y = <InlineKeyboardButton.CallbackButton>x[i];
 
       if (y.callback_data.includes(callbackData)) {
         return true;
@@ -140,4 +137,90 @@ export function hasButton(
 
 export function getUserLink(id: number) {
   return `<a href="tg://user?id=${id}">${id}</>`;
+}
+
+function escape(s: string) {
+  s = s.replace(/&/g, "&amp;");
+  s = s.replace(/</g, "&lt;");
+  s = s.replace(/>/g, "&gt;");
+  s = s.replace(/"/g, "&quot;");
+  s = s.replace(/\'/g, "&#x27;");
+  return s;
+}
+
+// Taken from Telethon with some modifications.
+export function unparse(
+  text: string,
+  entities: MessageEntity[],
+  offset = 0,
+  length?: number
+): string {
+  if (!text) return text;
+  else if (entities.length == 0) return escape(text);
+
+  length = length ?? text.length;
+
+  const html = [];
+  let lastOffset = 0;
+
+  for (let i = 0; i < entities.length; i++) {
+    const entity = entities[i];
+
+    if (entity.offset >= offset + length) break;
+
+    const relativeOffset = entity.offset - offset;
+
+    if (relativeOffset > lastOffset)
+      html.push(escape(text.slice(lastOffset, relativeOffset)));
+    else if (relativeOffset < lastOffset) continue;
+
+    let skipEntity = false;
+    const length_ = entity.length;
+    const text_ = unparse(
+      text.slice(relativeOffset, relativeOffset + length_),
+      entities.slice(i + 1, entities.length)
+    );
+
+    switch (entity.type) {
+      case "bold":
+        html.push(`<b>${text_}</b>`);
+        break;
+      case "italic":
+        html.push(`<i>${text_}</i>`);
+        break;
+      case "underline":
+        html.push(`<u>${text_}</u>`);
+        break;
+      case "strikethrough":
+        html.push(`<strike>${text_}</strike>`);
+        break;
+      case "text_link":
+        html.push(`<a href="${entity.url}">${text_}</a>`);
+        break;
+      case "text_mention":
+        html.push(`<a href="tg://user?id=${entity.user.id}">${text_}</a>`);
+        break;
+      case "spoiler":
+        html.push(`<span class="tg-spoiler">${text_}</span>`);
+        break;
+      case "code":
+        html.push(`<code>${text_}</code>`);
+        break;
+      case "pre":
+        html.push(
+          `<pre${
+            entity.language && ` class="${entity.language}"`
+          }>${text_}</pre>`
+        );
+        break;
+      default:
+        skipEntity = true;
+    }
+
+    lastOffset = relativeOffset + (skipEntity ? 0 : length_);
+  }
+
+  html.push(escape(text.slice(lastOffset, text.length)));
+
+  return html.join("");
 }
