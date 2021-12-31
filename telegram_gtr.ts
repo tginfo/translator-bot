2f455f83c8d8559857b9413b06853489ae91954c
Roj <ez@roj.im>
2022-01-01 00:20:36 +0300

    Create extended GTR class for use with entities

---
+++ telegram_gtr.ts
@@ -0,0 +1,20 @@
+import { GTR, TranslateOptions } from "https://deno.land/x/gtr/mod.ts";
+import { MessageEntity } from "https://cdn.skypack.dev/@grammyjs/types@v2.5.0?dts";
+import { unparse } from "./utils.ts";
+
+export class TelegramGTR extends GTR {
+  async translate(
+    text: string,
+    opts: TranslateOptions & { entities: MessageEntity[] }
+  ) {
+    const result = await super.translate(unparse(text, opts.entities), opts);
+
+    result.trans = result.trans
+      .replace(/> /g, ">")
+      .replace(/# /g, "#")
+      .replace(/ < \/ a>/g, "</a>")
+      .replace(/< \/ a>/g, "</a>");
+
+    return result;
+  }
+}
