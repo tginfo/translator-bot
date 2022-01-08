77bb310ac56d19de50537a4a706d092f3542f9d6
Roj <ez@roj.im>
2022-01-08 22:39:35 +0300

    Remove FileHandler

--- env.ts
+++ env.ts
@@ -3,5 +3,4 @@ import { cleanEnv, str } from "https://deno.land/x/envalid/mod.ts";
 
 export default cleanEnv(Deno.env.toObject(), {
   BOT_TOKEN: str(),
-  LOG_FILE: str({ default: "logs/logs.txt" }),
 });
--- logger.ts
+++ logger.ts
@@ -1,8 +1,6 @@
 import * as log from "https://deno.land/std@0.119.0/log/mod.ts";
 import { LogRecord } from "https://deno.land/std@0.119.0/log/mod.ts";
 
-import env from "./env.ts";
-
 const formatter = (logRecord: LogRecord) => {
   const date = new Date();
 
@@ -15,15 +13,11 @@ export const setup = () =>
       console: new log.handlers.ConsoleHandler("DEBUG", {
         formatter,
       }),
-      file: new log.handlers.FileHandler("DEBUG", {
-        filename: env.LOG_FILE,
-        formatter,
-      }),
     },
     loggers: {
       default: {
         level: "DEBUG",
-        handlers: ["console", "file"],
+        handlers: ["console"],
       },
     },
   });
