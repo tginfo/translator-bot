558a7e136834850a22f85b03c061d21832951acc
Roj <ez@roj.im>
2022-09-17 22:33:45 +0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -5,25 +5,24 @@ import {
   GrammyError,
   InlineKeyboard,
   InputFile,
-} from "https://deno.land/x/grammy@v1.9.1/mod.ts";
+} from "https://deno.land/x/grammy@v1.11.0/mod.ts";
+export {
+  type InlineKeyboardButton,
+  type InlineKeyboardMarkup,
+  type MessageEntity,
+} from "https://deno.land/x/grammy@v1.11.0/types.ts";
 import {
   FileFlavor,
   hydrateFiles,
 } from "https://deno.land/x/grammy_files@v1.0.4/mod.ts";
 export * as p from "https://deno.land/x/grammy_parse_mode@1.3.1/mod.ts";
 
-export * as log from "https://deno.land/std@0.149.0/log/mod.ts";
-export { MINUTE } from "https://deno.land/std@0.149.0/datetime/mod.ts";
-export { setColorEnabled } from "https://deno.land/std@0.149.0/fmt/colors.ts";
+export * as log from "https://deno.land/std@0.156.0/log/mod.ts";
+export { MINUTE } from "https://deno.land/std@0.156.0/datetime/mod.ts";
+export { setColorEnabled } from "https://deno.land/std@0.156.0/fmt/colors.ts";
 
-export {
-  type InlineKeyboardButton,
-  type InlineKeyboardMarkup,
-  type MessageEntity,
-} from "https://deno.land/x/grammy@v1.9.1/platform.deno.ts";
-
-export { config } from "https://deno.land/std@0.149.0/dotenv/mod.ts";
-export { cleanEnv, num, str } from "https://deno.land/x/envalid@v0.0.3/mod.ts";
+export { config } from "https://deno.land/std@0.156.0/dotenv/mod.ts";
+export { cleanEnv, num, str } from "https://deno.land/x/envalid@0.1.2/mod.ts";
 
 export {
   GTR,
