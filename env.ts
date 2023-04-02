5f8a9ce31d0e6f6ad865bc7c06b2ec2d71376ad1
Roj <ez@roj.im>
2023-04-02 18:17:01 +0300

    Update deps

--- env.ts
+++ env.ts
@@ -1,7 +1,5 @@
-import { config } from "std/dotenv/mod.ts";
+import "std/dotenv/load.ts";
 import { cleanEnv, num, str } from "envalid/mod.ts";
-
-await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
   BOT_TOKEN: str(),
