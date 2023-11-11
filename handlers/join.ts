d53a591eb86069fd862e823f34c0e9d7fdf9fa41
Roj <ez@roj.im>
2023-11-11 12:04:21 +0300

    Automate this

--- handlers/join.ts
+++ handlers/join.ts
@@ -1,6 +1,7 @@
+import * as log from "std/log/mod.ts";
 import { Composer } from "grammy/mod.ts";
 import { Context } from "../utils.ts";
-import { INLINE_INVITE_LINK_NAME } from "../data.ts";
+import { INLINE_INVITE_LINK_NAME, languages, update } from "../data.ts";
 
 const composer = new Composer<Context>();
 
@@ -11,11 +12,27 @@ composer.on("chat_member", async (ctx) => {
     ctx.chatMember.invite_link &&
     ctx.chatMember.invite_link.name == INLINE_INVITE_LINK_NAME
   ) {
-    await ctx.promoteAuthor({
-      can_delete_messages: true,
-      can_edit_messages: true,
-      can_pin_messages: true,
-      can_post_messages: true,
-    });
+    const lang = Object.entries(languages).find((v) => v[1].edit == ctx.chat.id)
+      ?.[0];
+    if (lang) {
+      if (languages[lang].translators.includes(ctx.chatMember.from.id)) {
+        log.info(
+          `Automatic promotion: ${ctx.chatMember.from.id} is already a translator in ${lang}.`,
+        );
+        return;
+      }
+      await ctx.promoteAuthor({
+        can_delete_messages: true,
+        can_edit_messages: true,
+        can_pin_messages: true,
+        can_post_messages: true,
+      });
+      await update((languages) => {
+        languages[lang].translators.push(ctx.chatMember.from.id);
+      });
+      log.info(
+        `Automatic promotion: Promoted ${ctx.chatMember.from.id} in ${lang}.`,
+      );
+    }
   }
 });
