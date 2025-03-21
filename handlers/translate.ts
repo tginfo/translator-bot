838cdf2d18db7a7249c691eb9bdf50b4eb2c1547
Roj <ez@roj.im>
2025-03-21 23:46:22 +0300

    Ignore #промо

--- handlers/translate.ts
+++ handlers/translate.ts
@@ -26,7 +26,7 @@ composer
 
     if (
       ctx.entities("hashtag").some((v) =>
-        ["#df", "#реклама"].includes(v.text.toLowerCase())
+        ["#df", "#реклама", "#промо"].includes(v.text.toLowerCase())
       )
     ) {
       log.info(`Ignored ${postId}.`);
